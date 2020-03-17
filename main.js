6ef750a1389ad965e3038efbf6e0e3ca9c60f91c
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-17 17:25:35 +0100

    end done

---
+++ main.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
+const liElements = document.querySelectorAll('li');
+
+const searchTask = (e) => {
+    const searchText = e.target.value.toLowerCase();
+    let tasks = [...liElements];
+    console.log(tasks);
+    tasks = tasks.filter(li => li.textContent.toLowerCase().includes(searchText));
+    ul.textContent = "";
+    tasks.forEach(li => ul.appendChild(li));
+}
+
+input.addEventListener('input', searchTask);
